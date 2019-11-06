fb72c2b55a1cc81ae25a785f2656be3dd17c1840
ryanpradnya <ryanpradnya@gmail.com>
2019-11-06 22:00:12 +0700

    Fetching job name and company name

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const cheerio = require('cheerio');
 request('https://www.urbanhire.com/jobs', (error, response, html) => {
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
+        let jobName = [];
+        let companyName = [];
+        let jobLocation = [];
+        let jobFuction = [];
+        let jobIndustry = [];
 
         $('.m-b-lg').each((i, el) => {
-            const title = $(el)
-                .find('h2')
-                .text();
-            // .replace(/\s\s+/g, '');
-            // const link = $(el)
-            //     .find('a')
-            //     .attr('href');
-            // const date = $(el)
-            //     .find('.post-date')
-            //     .text()
-            //     .replace(/,/, '');
+            $(el).find('h2').each((j, data) => {
+                jobName[j] = $(data).text();
+            });
+            $(el).find('h3').each((j, data) => {
+                companyName[j] = $(data).text();
+            });
 
-            console.log(title)
         });
+        console.log(jobName);
+        console.log(companyName);
 
         console.log('Scraping Done...');
     }
